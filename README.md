56fd7764f9e15d77e8895cadec225fd084c1632f
team Dandelion Mané <git@dandelion.art>
2024-01-15 13:35:40 -0800

    Initial commit: working bot with slash commands

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # Meadows Quest Questbot
 
 A Discord bot for creating, updating, and playing with quests, as tracked via GitHub issues.
+
+# Discord Bot
+
+Very simple discord bot created per [these docs](https://discordjs.guide/creating-your-bot/).
+
+The code is written to assume we're running the JS transpiled files, not the TS source. So:
+First run `tsc` to compile everything
+Then run `node index.js` to run the bot.
--- index.ts
+++ index.ts
@@ -1,15 +1,79 @@
+import fs from "node:fs";
+import path from "node:path";
+
 // Require the necessary discord.js classes
-const { Client, Events, GatewayIntentBits } = require("discord.js");
-const { token } = require("./config.json");
+import { Client, Events, GatewayIntentBits, Collection } from "discord.js";
+import { token } from "./config.json";
+
+type ClientWithCommands<Boolean> = Client & {
+  commands: Collection<string, any>;
+};
+
+function c(): ClientWithCommands<Boolean> {
+  const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+  (client as ClientWithCommands<Boolean>).commands = new Collection();
+  return client as ClientWithCommands<Boolean>;
+}
 
 // Create a new client instance
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+const client: ClientWithCommands<Boolean> = c();
+
+const foldersPath = path.join(__dirname, "commands");
+const commandFolders = fs.readdirSync(foldersPath);
+for (const folder of commandFolders) {
+  const commandsPath = path.join(foldersPath, folder);
+  const commandFiles = fs
+    .readdirSync(commandsPath)
+    .filter((file) => file.endsWith(".js"));
+  for (const file of commandFiles) {
+    const filePath = path.join(commandsPath, file);
+    const command = require(filePath);
+    // Set a new item in the Collection with the key as the command name and the value as the exported module
+    if ("data" in command && "execute" in command) {
+      client.commands.set(command.data.name, command);
+    } else {
+      console.log(
+        `[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`
+      );
+    }
+  }
+}
 
 // When the client is ready, run this code (only once).
 // The distinction between `client: Client<boolean>` and `readyClient: Client<true>` is important for TypeScript developers.
 // It makes some properties non-nullable.
-client.once(Events.ClientReady, (readyClient) => {
+client.once(Events.ClientReady, (readyClient: Client<true>) => {
   console.log(`Ready! Logged in as ${readyClient.user.tag}`);
+});
+
+client.on(Events.InteractionCreate, async (interaction) => {
+  if (!interaction.isChatInputCommand()) return;
+
+  const command = (interaction.client as ClientWithCommands<true>).commands.get(
+    interaction.commandName
+  );
+
+  if (!command) {
+    console.error(`No command matching ${interaction.commandName} was found.`);
+    return;
+  }
+
+  try {
+    await command.execute(interaction);
+  } catch (error) {
+    console.error(error);
+    if (interaction.replied || interaction.deferred) {
+      await interaction.followUp({
+        content: "There was an error while executing this command!",
+        ephemeral: true,
+      });
+    } else {
+      await interaction.reply({
+        content: "There was an error while executing this command!",
+        ephemeral: true,
+      });
+    }
+  }
 });
 
 // Log in to Discord with your client's token
